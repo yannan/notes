3e789c350ca22283a7555792d22861b1d444aa7f
yannan <yannan@021.com>
2017-12-19 09:47:28 +0800

    最近提交

---
+++ spider-client/src/component/tab-common.vue
@@ -0,0 +1,31 @@
+<template lang="html">
+  <div class="table-common">
+    <ul>
+      <li v-for="info in infos">
+        {{ info.info }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'tablist',
+  data () {
+    return {
+      infos: [
+        {'info':1},
+        {'info':2},
+        {'info':3},
+        {'info':4}
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.table-common {
+
+}
+</style>
