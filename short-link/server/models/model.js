83c708b87acfed8a82b07cd1e6cc56dba2c98640
yannan <yannan@021.com>
2018-01-18 20:33:58 +0800

    短网址文档更新

--- short-link/server/models/model.js
+++ short-link/server/models/model.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 
+let counter = 1;
+let CountedId = {type: Number, default: () => counter++};
+
 const linkSchema = new mongoose.Schema({
   originLink: String,
   shortLink: String,
